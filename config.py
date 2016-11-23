b18e9c3d52dabfb68bc050cea3f8119e895a076f
Barbara Gabriela <barbi.garza@gmail.com>
2016-11-23 12:41:48 +0100

    Add config file

---
+++ config.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+TOKEN = os.environ.get('TOKEN',"")
+
+URBAN_URL = 'http://api.urbandictionary.com/v0/define?'
+URBAN_WEB_URL = 'http://www.urbandictionary.com/define.php?'
+
+HELP = """
+urbanBot helps you query urbandictionary from the commodity of your telegram chat 👻👻👻
+
+to define something use the ```/define``` command.
+"""
+
